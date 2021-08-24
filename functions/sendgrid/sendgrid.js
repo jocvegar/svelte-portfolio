df6de90ac00131440ff45732cd960d4639924148
Jose Vega <jocvegar@gmail.com>
2021-08-24 16:00:50 -0600

    test sendgrid 2

--- functions/sendgrid/sendgrid.js
+++ functions/sendgrid/sendgrid.js
@@ -42,7 +42,7 @@ exports.handler = function (event, context, callback) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: "jocvegar@gmail.com",
-    from: "jocvegar@gmail.com",
+    from: "josevegaraquel@gmail.com",
     subject: "Sending with Twilio SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
